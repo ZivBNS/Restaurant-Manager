b8613f0f737e673ccf68e5d799ead94c5d6e4d10
ZivBNS <zivbs123@gmail.com>
2026-02-16 18:13:24 +0200

    Revert "Final Update"
    This reverts commit 69dfc6454792b6d41bce648aa424b53a2b10b2db.

--- G5_Assignment3/JAR/RunClient.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-java -Djava.library.path="G5_client_lib" --module-path "G5_client_lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics -cp "G5_client.jar;G5_client_lib/*" gui.Client_GUI
--- G5_Assignment3/JAR/‏‏RunServer.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-java -Djava.library.path="G5_server_lib" --module-path "G5_server_lib" --add-modules javafx.controls,javafx.fxml,javafx.graphics -cp "G5_server.jar;G5_server_lib/*" gui.Server_GUI
